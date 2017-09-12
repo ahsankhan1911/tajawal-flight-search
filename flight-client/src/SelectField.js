5757e2bd61e3bcef25531b515e4916b705ac6d13
Ahsan Khan <ahsankhan1911@gmail.com>
2017-09-12 13:00:59 +0500

    workk

--- flight-client/src/SelectField.js
+++ flight-client/src/SelectField.js
@@ -1,18 +1,40 @@
 import React, {Component} from 'react';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import { inject, observer } from 'mobx-react';
 import './style.css'
 
+@inject('FlightData')
+@observer class SelectFieldUI extends Component {
+//   constructor() {
+//     super();
 
-var devstyle ={
-  display: 'inline'
-}
-export default class SelectFieldUI extends Component {
-  state = {
-    value: 1,
-  };
+//     this.state = {
+//         value:1,
+//         adults: 0
+//     };
 
+//     this.handleInc = this.handleInc.bind(this);
+//     this.handleDec = this.handleDec.bind(this);
+// }
+       t
   handleChange = (event, index, value) => this.setState({value});
+
+  handleInc() {
+     this.setState({
+       adults: this.state.adults+1
+     })
+
+    }
+
+    handleDec() {
+      this.setState({
+        adults: this.state.adults-1
+      })
+ 
+     }
+    
+
 
   render() {
     return (
@@ -24,17 +46,29 @@ export default class SelectFieldUI extends Component {
         <MenuItem value={4} label="First" primaryText="First" />
       </SelectField><br/>
       <SelectField value={this.state.value} onChange={this.handleChange}>
-        <table>
-         <tbody> 
-           <tr>
-          <td> Adults(12+)  <div ><input className="input1" type="button" value="-"/>  <span>4</span> <input type="button" value="-"/></div></td>
-          <td><p>Chuldren(2- 12)</p>     <input type="button" value="-"/>    <p></p> </td>
-          <td> <p>Infant(0-2)</p></td>
-          </tr>
-        </tbody>
-        </table>
+
+        <table className="table1" cellPadding="15px">
+          <tbody>
+  <tr>
+    <td>Adults(12+)</td>
+    <td><input className="input1" type="button" value="-" onClick={this.handleDec}/>  <span>{this.state.adults}</span> <input className="input1" type="button" value="+" onClick={this.handleInc}/></td> 
+  </tr>
+  <tr>
+    <td>Children(2- 12)</td>
+    <td><input className="input1" type="button" value="-"/>  <span>4</span> <input className="input1" type="button" value="+"/></td> 
+
+  </tr>
+  <tr>
+    <td>Infant(0-2)</td>
+    <td><input className="input1" type="button" value="-"/>  <span>4</span> <input className="input1" type="button" value="+"/></td> 
+  </tr>
+  </tbody>
+</table>
     </SelectField>
     </div>
     );
   }
 }
+
+
+export default SelectFieldUI
---
+++ flight-client/src/stores/Appstate.js
@@ -0,0 +1,11 @@
+import { observable, action } from 'mobx';
+
+class FlightData {
+
+    @obversable value = 1;
+
+    @obversable adults = 0;
+ }
+
+
+export default FlightData;
